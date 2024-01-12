f7b561800c2f91492dfc06aa23da2604953773f7
Dima <dimoakim730@icloud.com>
2024-01-12 12:46:32 +0300

    homework - 1 update

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,10 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+import pytest
+from src.item import Item
+
+
+@pytest.fixture()
+def test_calculate_total_price():
+    assert Item("Смартфон", 10000, 20)
+    assert Item("Ноутбук", 20000, 5)
